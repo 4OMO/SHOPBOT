27cf19894f5e0e5fb3859bde7edc11786ca7a88c
4OMO <160089212+4OMO@users.noreply.github.com>
2024-02-15 11:32:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+TOKEN = "SECRET_TOKEN"
+#   https://t.me/F4OMO_BOT
+
+ADMIN = "ZeroXCryptoKai"         
+
+ADMIN_ID = 5139927253   
+
+CONTACT_LINK = "https://4omo.co/pages/contact-us"
+
+PAYMENT_MSG = """
+To make your payment hassle-free, kindly send the USD equivalent in cryptocurrency to the provided address, ensuring that 
+transaction fees are covered within the sent funds. If the amount sent falls short due to these fees, it will be refunded 
+for a smooth and complete transaction experience.
+
+Bitcoin (BTC): <code>bc1qvrlyecgn7wq3gd2v663yv058aumpt75qsu23f3</code>
+
+Solana (SOL): <code>CsNxhnef3f2m51YvSkTuWfbkfhUyS567FBiaokZ18y59</code>
+
+Ethereum (ETH): <code>0xD05b5643411CFEE2563462A607Ca81D5A603A334</code>
+
+Tether USDT (ERC20): <code>0xD05b5643411CFEE2563462A607Ca81D5A603A334</code>
+
+Tether USDT (BSC): <code>0xD05b5643411CFEE2563462A607Ca81D5A603A334</code>
+
+USDC (Ethereum/ERC20): <code>0xD05b5643411CFEE2563462A607Ca81D5A603A334</code>
+
+USDC (Solana Network): <code>CsNxhnef3f2m51YvSkTuWfbkfhUyS567FBiaokZ18y59</code>
+
+<b>NOTE: Please provide the Transaction Hash of your payment after completing the payment process.</b>
+"""
